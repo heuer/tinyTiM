8b6b341b14e45eeea2cc004fdc857a05b60efc01
Lars <heuer@users.noreply.github.com>
2008-04-28 13:19:37 +0000

    - Moved test / main into the right subdir

--- tinytim-mio/trunk/src/main/java/org/tinytim/mio/MapInputHandler.java
+++ tinytim-mio/trunk/src/main/java/org/tinytim/mio/MapInputHandler.java
@@ -46,7 +46,7 @@ import com.semagia.mio.MIOException;
  * {@link com.semagia.mio.IMapHandler} implementation.
  * 
  * @author Lars Heuer (heuer[at]semagia.com) <a href="http://www.semagia.com/">Semagia</a>
- * @version $Rev:$ - $Date:$
+ * @version $Rev$ - $Date$
  */
 public class MapInputHandler implements IMapHandler {
 
