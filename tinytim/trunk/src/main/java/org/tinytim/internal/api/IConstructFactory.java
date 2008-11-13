3f21ba33b28c8f00459339f86cb49a524031ddfc
Lars <heuer@users.noreply.github.com>
2008-11-13 15:33:33 +0000

    Docs for the IConstructFactory

--- tinytim/trunk/src/main/java/org/tinytim/internal/api/IConstructFactory.java
+++ tinytim/trunk/src/main/java/org/tinytim/internal/api/IConstructFactory.java
@@ -19,23 +19,62 @@ import org.tmapi.core.Association;
 import org.tmapi.core.Role;
 
 /**
- * 
+ * Factory for {@link IConstruct}s.
+ * <p>
+ * This interface is not meant to be used outside of the tinyTiM package.
+ * </p>
  * 
  * @author Lars Heuer (heuer[at]semagia.com) <a href="http://www.semagia.com/">Semagia</a>
  * @version $Rev:$ - $Date:$
  */
 public interface IConstructFactory {
 
+    /**
+     * Creates a topic attached to the topic map this factory belongs to.
+     *
+     * @return A topic with no identity.
+     */
+    public ITopic createTopic();
+
+    /**
+     * Creates an association attached to the topic map this 
+     * factory belongs to.
+     *
+     * @return An association with no type, scope, and roles.
+     */
     public Association createAssociation();
 
+    /**
+     * Creates a role attached to the specified <tt>parent</tt>.
+     *
+     * @param parent The parent of the role.
+     * @return A role with no type and player.
+     */
     public Role createRole(Association parent);
 
-    public IVariant createVariant(IName parent);
-
+    /**
+     * Creates an occurrence attached to the specified <tt>parent</tt>.
+     *
+     * @param parent The parent of the occurrence.
+     * @return An occurrence with no type, literal, and scope.
+     */
     public IOccurrence createOccurrence(ITopic parent);
 
+    /**
+     * Creates a name attached to the specified <tt>parent</tt>.
+     *
+     * @param parent The parent of the name.
+     * @return A name with no type, literal, and scope.
+     */
     public IName createName(ITopic parent);
 
-    public ITopic createTopic();
+    /**
+     * Creates a variant attached to the specified <tt>parent</tt>.
+     *
+     * @param parent The parent of the variant.
+     * @return A variant with the parent's scope, but no own scope and 
+     *          no literal.
+     */
+    public IVariant createVariant(IName parent);
 
 }
