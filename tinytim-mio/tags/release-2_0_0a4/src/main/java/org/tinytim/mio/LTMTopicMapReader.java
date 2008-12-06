41a029f0bc1637d6f4b42aeeae06a97f2a99ccb9
lheuer <lheuer@3b9cb24b-fc4a-0410-becc-d259e82b0014>
2008-12-06 17:22:07 +0000

    Tagged for release

---
+++ tinytim-mio/tags/release-2_0_0a4/src/main/java/org/tinytim/mio/LTMTopicMapReader.java
@@ -0,0 +1,148 @@
+/*
+ * Copyright 2008 Lars Heuer (heuer[at]semagia.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.tinytim.mio;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.tmapi.core.TopicMap;
+
+import com.semagia.mio.Source;
+import com.semagia.mio.Syntax;
+
+/**
+ * {@link TopicMapReader} implementation that is able to deserialize the
+ * <a href="http://www.ontopia.net/download/ltm.html">Linear Topic Map Notation (LTM) 1.3</a>.
+ * This deserializer handles LTM instances somewhat different from the official
+ * specification:
+ * <ul>
+ *  <li>Identifiers are not limited to <tt>[A-Za-z0-9_-.]</tt>, but may 
+ *      contain Unicode characters.</li>
+ *  <li>Roles are not postprocessed; if a role does not have a type, the type 
+ *      is set to a default role type.</li>
+ *  <li>Multiple subject locators are allowed: If a topic contains more than
+ *      one subject locator, the subject locator is added to the topic.</li>
+ *  <li>Subject identifiers / locators may occur in any order: The LTM 
+ *      specification seems to mandate, that the subject identifiers must be 
+ *      specified after an subject locator (if given); this implementation 
+ *      does not mandate any order; a subject locator may be followed by an 
+ *      subject identifier etc.</li>
+ * </ul>
+ * </p>
+ * 
+ * @author Lars Heuer (heuer[at]semagia.com) <a href="http://www.semagia.com/">Semagia</a>
+ * @version $Rev$ - $Date$
+ */
+public final class LTMTopicMapReader extends AbstractTopicMapReader {
+
+    /**
+     * Constructs a new instance.
+     *
+     * @param topicMap The topic map to which the content is added to.
+     * @param source The source to read the topic map from.
+     * @param docIRI The document IRI which is used to resolve IRIs against.
+     * @throws IOException If an error occurs. 
+     */
+    public LTMTopicMapReader(final TopicMap topicMap, final File source,
+            final String docIRI) throws IOException {
+        super(topicMap, Syntax.LTM, source, docIRI);
+    }
+
+    /**
+     * Constructs a new instance.
+     * <p>
+     * The <tt>source</tt> is converted into an absolute IRI which will be
+     * utilised as document IRI
+     * </p>
+     *
+     * @param topicMap The topic map to which the content is added to.
+     * @param source The source to read the topic map from.
+     * @throws IOException If an error occurs. 
+     */
+    public LTMTopicMapReader(final TopicMap topicMap, final File source)
+            throws IOException {
+        super(topicMap, Syntax.LTM, source);
+    }
+
+    /**
+     * Constructs a new instance.
+     *
+     * @param topicMap The topic map to which the content is added to.
+     * @param source The source to read the topic map from.
+     * @param docIRI The document IRI which is used to resolve IRIs against.
+     */
+    public LTMTopicMapReader(final TopicMap topicMap, final InputStream source,
+            final String docIRI) {
+        super(topicMap, Syntax.LTM, source, docIRI);
+    }
+
+    /**
+     * Constructs a new instance.
+     *
+     * @param topicMap The topic map to which the content is added to.
+     * @param source The source to read the serialized topic map from.
+     */
+    public LTMTopicMapReader(final TopicMap topicMap, final Source source) {
+        super(topicMap, Syntax.LTM, source);
+    }
+
+//    /**
+//     * Enables / disables processing of the "#MERGEMAP" directive.
+//     * <p>
+//     * The reader won't deserialize topic maps referenced by mergeMap if
+//     * this feature is enabled (disabled by default).
+//     * </p>
+//     *
+//     * @param ignore <tt>true</tt> to ignore "#MERGEMAP" directives, 
+//     *                  otherwise <tt>false</tt>.
+//     */
+//    public void setIgnoreMergeMap(boolean ignore) {
+//        _deserializer.setProperty(Property.IGNORE_MERGEMAP, ignore);
+//    }
+//
+//    /**
+//     * Returns if this reader ignores "#MERGEMAP" directives.
+//     *
+//     * @return <tt>true</tt> if "#MERGEMAP" is ignored, otherwise <tt>false</tt>.
+//     */
+//    public boolean isIgnoringMergeMap() {
+//        return Boolean.TRUE.equals(_deserializer.getProperty(Property.IGNORE_MERGEMAP));
+//    }
+
+//    /**
+//     * Enables / disables processing of the "#INCLUDE" directive.
+//     * <p>
+//     * The reader won't deserialize topic maps referenced by "#INCLUDE" if
+//     * this feature is enabled (disabled by default).
+//     * </p>
+//     *
+//     * @param ignore <tt>true</tt> to ignore "#INCLUDE" directives, 
+//     *                  otherwise <tt>false</tt>.
+//     */
+//    public void setIgnoreInclude(boolean ignore) {
+//        _deserializer.setProperty(Property.IGNORE_INCLUDE, ignore);
+//    }
+//
+//    /**
+//     * Returns if this reader ignores "#INCLUDE" directives.
+//     *
+//     * @return <tt>true</tt> if "#INCLUDE" is ignored, otherwise <tt>false</tt>.
+//     */
+//    public boolean isIgnoringInclude() {
+//        return Boolean.TRUE.equals(_deserializer.getProperty(Property.IGNORE_INCLUDE));
+//    }
+}
---
+++ tinytim-mio/tags/release-2_0_0a4/src/main/java/org/tinytim/mio/TopicMapReader.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2008 Lars Heuer (heuer[at]semagia.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.tinytim.mio;
+
+import java.io.IOException;
+
+/**
+ * This interface represents a reader to deserialize a topic map from a source.
+ * <p>
+ * The reader is not meant to be reused and should be thrown away once the 
+ * {@link #read()} method was invoked.
+ * </p>
+ * 
+ * @author Lars Heuer (heuer[at]semagia.com) <a href="http://www.semagia.com/">Semagia</a>
+ * @version $Rev$ - $Date$
+ */
+public interface TopicMapReader {
+
+    /**
+     * Reads a topic map.
+     *
+     * @throws IOException If an error occurs.
+     */
+    public void read() throws IOException;
+
+}
