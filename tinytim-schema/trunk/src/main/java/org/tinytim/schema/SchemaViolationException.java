d7a22e40de2d61d366d3c623866e4ea6e1544936
lheuer <lheuer@3b9cb24b-fc4a-0410-becc-d259e82b0014>
2009-04-03 11:35:36 +0000

    Initial import of the schema interfaces

---
+++ tinytim-schema/trunk/src/main/java/org/tinytim/schema/SchemaViolationException.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2009 Lars Heuer (heuer[at]semagia.com). All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.tinytim.schema;
+
+import org.tmapi.core.Construct;
+
+/**
+ * Exception thrown in case of a schema violation.
+ * 
+ * @author Lars Heuer (heuer[at]semagia.com) <a href="http://www.semagia.com/">Semagia</a>
+ * @version $Rev:$ - $Date:$
+ */
+public class SchemaViolationException extends SchemaException {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -9188289535093661832L;
+
+    /**
+     * The construct which violates the schema.
+     */
+    private final Construct _construct;
+
+    /**
+     * Initializes a new instance.
+     *
+     * @param message The message.
+     * @param construct The construct which violates the schema, must not be <tt>null</tt>.
+     */
+    public SchemaViolationException(String message, Construct construct) {
+        super(message);
+        if (construct == null) {
+            throw new IllegalArgumentException("The construct must not be null");
+        }
+        _construct = construct;
+    }
+
+    /**
+     * Returns the construct which has violates the {@link Schema}.
+     *
+     * @return The construct which violates the schema.
+     */
+    public Construct getConstruct() {
+        return _construct;
+    }
+}
